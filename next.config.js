e9d1cf4082a3df9f584812bf6764ebf6bef93bc7
Johnrycgm <johnry@cgmland.com>
2025-08-15 04:12:02 -0700

    Update next.config.js to add alias for components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    // Add alias for root and components directory to resolve imports like '@components/ui/button'
+    config.resolve.alias['@'] = __dirname;
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
